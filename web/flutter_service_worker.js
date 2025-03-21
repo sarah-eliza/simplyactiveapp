6878588cec36d6864d4ef3c4eea036054d8418bd
Sarah Stutts <sarahstutts@Sarahs-MacBook-Pro.local>
2025-03-21 16:33:31 -0700

    Deploy updated Flutter web build with separate timer pages

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -3,17 +3,13 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9a24e2ef3c96046d3b6ca1deb0fe3466",
+const RESOURCES = {"flutter_bootstrap.js": "09f396df36545c0d392c4277fbfa4216",
 "version.json": "c8271ac8eec35ed1e6db8074f7537c52",
 "index.html": "c584193bea7903d701c51f5415971723",
 "/": "c584193bea7903d701c51f5415971723",
-"main.dart.js": "b7ba860dc4fbf703a40635bb240d08d9",
+"main.dart.js": "846a39b085a1a835d3a5abffe7857b0e",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "8fa86c49aec030ee4a334ecd3f135f35",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "a9a47fd7c2f07d562382e3f15e15245e",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/NOTICES": "3bc05c5d064652ab4eddf68b7ab5f406",
